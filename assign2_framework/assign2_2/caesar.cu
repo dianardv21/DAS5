10994a5136b929e043cefff948cbfd2a0dbee0f0
DanieldenHartog <danielmdenhartog@gmail.com>
2023-11-28 12:07:00 +0100

    d

--- assign2_framework/assign2_2/caesar.cu
+++ assign2_framework/assign2_2/caesar.cu
@@ -48,10 +48,10 @@ __global__ void encryptKernel(int n, char* deviceDataIn, int key_length, int *ke
     //} //in case key cant be directly mapped to ASCII code
 
 
-    *key = *key % 256;
-    if (i < 4) // don't calculate non-existing data points
+    //*key = *key % 256;
+    if (i < n) // don't calculate non-existing data points
     {  
-        deviceDataOut[i] = (deviceDataIn[i] + *key) % 256;
+        deviceDataOut[i] = deviceDataIn[i]// + *key) % 256;
     }
 }
 
