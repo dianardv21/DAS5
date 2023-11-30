125ccc19e8ee3a7259c1990896f9d8a5e7c7dbeb
DanieldenHartog <danielmdenhartog@gmail.com>
2023-11-30 10:35:04 +0100

    sim

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -1,26 +1,12 @@
-/*
- * simulate.c
- *
- * Implement your (parallel) simulation here!
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <mpi.h>
 #include "simulate.h"
 
 
 /* Add any global variables you may need. */
 
-
-/* Add any functions you may need (like a worker) here. */
-
-
 /*
- * Executes the entire simulation.
- *
- * Implement your code here.
- *
  * i_max: how many data points are on a single wave
  * t_max: how many iterations the simulation should run
  * old_array: array of size i_max filled with data for t-1
@@ -29,12 +15,23 @@
  */
 double *simulate(const int i_max, const int t_max, double *old_array,
         double *current_array, double *next_array)
-{
+{   
+    int size, comm_rank;
+    int num_procs = 1;
     
-    /*
-     * Your implementation should go here.
-     */
+    MPI_Init();
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
 
-    /* You should return a pointer to the array with the final results. */
+    //partition the data in chunks
+
+
+    // sync block: MPI_Ssend - nonblock: MPI_ISsend
+    // buffered block: MPI_Bsend - nonblock: MPI_IBsend
+    // ready mode block: MPI_Rsend - nonblock: MPI_IRsend
+    // MPI_Recv receives any send mode messages
+    // MPI_Barrier(comm) for all processes
+
+    MPI_Finalize();
     return current_array;
 }
