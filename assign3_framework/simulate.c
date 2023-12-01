d063f51870f1d4d0328d932fc2a16c6135c3e0ec
DanieldenHartog <danielmdenhartog@gmail.com>
2023-12-01 17:02:22 +0100

    d

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -84,7 +84,7 @@ double *simulate(const int i_max, const int t_max, double *old_array,
     }
     
 
-    double *buffer_array;
+    double *buffer_array; // buffer to store received array domains
     if(rank != 0) {
         // send all arrays to master process
         MPI_Send(&current_array, i_max, MPI_DOUBLE, 0,  rank, MPI_COMM_WORLD);//, &reqs[4]);
@@ -95,8 +95,9 @@ double *simulate(const int i_max, const int t_max, double *old_array,
     else {
         for (int i = 1; i < numprocs; i++) {
             // for each non-master process get domain and copy only the domain to current_array
-            
+            printf("here %i",rank);
             start = edges[i][0];
+            printf("here %i",rank);
             end = edges[i][1];
             printf("start: %i, end: %i, rank: %i", start, end, rank);
             MPI_Recv(&buffer_array, i_max, MPI_DOUBLE, i,  i, MPI_COMM_WORLD, &stats[5]);
