0cd9fbb55d870d788f1fd397ae3061e63999ee0f
DanieldenHartog <danielmdenhartog@gmail.com>
2023-12-01 11:48:18 +0100

    code for simulate

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -16,16 +16,57 @@
 double *simulate(const int i_max, const int t_max, double *old_array,
         double *current_array, double *next_array)
 {   
-    int size, comm_rank;
-    int num_procs = 1;
+    int numprocs, rank;
+    double c = 0.15;
+
+    MPI_Init(NULL,NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // TODO: determine data partitioning
+
+    // start iterations
+    for(int t = 0; t < t_max; t++) {
+
+        // send/recv halo cells, 
+        double left, right;
+        if (rank != numprocs-1) {
+            MPI_ISend(current_array[end], 1, MPI_DOUBLE, rank+1,  rank, MPI_COMM_WORLD); // send end to next as start-1
+            MPI_IRecv(&right, 1, MPI_DOUBLE, rank+1, rank+1, MPI_COMM_WORLD); // get start from next as end+1
+        } else {right = 0;} // edge of array is always 0
+        if(rank != 0) {
+            MPI_ISend(current_array[start], 1, MPI_DOUBLE, rank-1,  rank, MPI_COMM_WORLD); // send start to previous as end+1
+            MPI_IRecv(&left, 1, MPI_DOUBLE, rank-1, rank-1, MPI_COMM_WORLD); // get end from previous as start-1
+        } else {left = 0;} // edge of array is always 0
+        
+        // let computation run during communication
+        for(int i = start+1; i < end; i++) {
+            
+            next_array[i] = 2*current_array[i]-old_array[i]+c*(current_array[i-1]-(2*current_array[i]-current_array[i+1]));
+
+        }
+        // TODO: Make this MPI_Wait with request instead of barrier
+        MPI_Barrier(MPI_COMM_WORLD);
+        // handle halo cells
+        next_array[start] = 2*current_array[start]-old_array[start]+c*(left-(2*current_array[start]-current_array[start+1]));
+        next_array[end] = 2*current_array[end]-old_array[end]+c*(current_array[end-1](2*current_array[end]-right));
+        
+        // swap locally
+        double *temp = old_array;
+        old_array = current_array;
+        current_array = next_array;
+        next_array = temp;
+
+    }
+
     
-    MPI_Init();
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
+    //typedef struct MPI_STATUS {
+    //    int MPI_TAG;
+    //    int MPI_ERROR;
+    //    int MPI_SOURCE;
+    //}
 
-    //partition the data in chunks
-
-
+    // MPI_ANY_TAG
     // sync block: MPI_Ssend - nonblock: MPI_ISsend
     // buffered block: MPI_Bsend - nonblock: MPI_IBsend
     // ready mode block: MPI_Rsend - nonblock: MPI_IRsend
