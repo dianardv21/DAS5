0dc03b5512e2d534a9558ac916c0c393860a0b12
DanieldenHartog <danielmdenhartog@gmail.com>
2023-12-02 18:44:17 +0100

    d

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -208,11 +208,6 @@ double *simulate(const int i_max, const int t_max, double *old_array,
     printf("\ncount: %i, rank: %i\n", req_count, rank);
     MPI_Waitall(req_count, reqs, MPI_STATUS_IGNORE);
 
-    if (rank != 0) {
-        printf("\nSent start: %f to %i\nRank %i, left: %f, right: %f\n ", current_array[start],rank-1,rank,left,right); }
-    if (rank != numprocs-1) {
-        printf("\nSent end: %f to %i\nRank %i, left: %f, right: %f\n ", current_array[end],rank+1,rank,left,right);
-    }
     next_array[start] = 2*current_array[start]-old_array[start]+c*(left-(2*current_array[start]-current_array[start+1]));
     next_array[end] = 2*current_array[end]-old_array[end]+c*(current_array[end-1]-(2*current_array[end]-right));
 
