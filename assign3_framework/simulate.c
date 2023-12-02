a68df5fb00bb4faa45b9a289ca134640396c0a7e
DanieldenHartog <danielmdenhartog@gmail.com>
2023-12-02 18:09:10 +0100

    d

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -188,13 +188,13 @@ double *simulate(const int i_max, const int t_max, double *old_array,
     if (rank != numprocs-1) { // for leftmost process
         MPI_Isend(&current_array[end], 1, MPI_DOUBLE, rank+1,  rank, MPI_COMM_WORLD, &reqs[0]); // send end to next as start-1
         MPI_Irecv(&right, 1, MPI_DOUBLE, rank+1, rank+1, MPI_COMM_WORLD, &reqs[1]); // get start from next as end+1
-        req_count += 2;
+        req_count += 1;
     } else {right = 0;} // edge of array is always 0
 
     if (rank != 0) { // for rightmost process
         MPI_Isend(&current_array[start], 1, MPI_DOUBLE, rank-1,  rank, MPI_COMM_WORLD, &reqs[2]); // send start to previous as end+1
         MPI_Irecv(&left, 1, MPI_DOUBLE, rank-1, rank-1, MPI_COMM_WORLD, &reqs[3]); // get end from previous as start-1
-        req_count += 2;
+        req_count += 1;
     } else {left = 0;} // edge of array is always 0
     
     // let computation run during communication
