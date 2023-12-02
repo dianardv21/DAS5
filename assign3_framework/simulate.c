f206db42ce11a32463e679f26d8b9e29be46c094
DanieldenHartog <danielmdenhartog@gmail.com>
2023-12-02 14:06:02 +0100

    D

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -81,10 +81,11 @@ double *simulate(const int i_max, const int t_max, double *old_arra,
         next_array[start] = 2*current_array[start]-old_array[start]+c*(left-(2*current_array[start]-current_array[start+1]));
         next_array[end] = 2*current_array[end]-old_array[end]+c*(current_array[end-1]-(2*current_array[end]-right));
         
-        //for (int i=0;i<i_max;i++){
-        //    printf("%f  r: %i  i: %i  \n", current_array[i], rank, i);
-        //}
-        //printf("\n\n Proc: %i   t: %i \n", rank, t);
+        if(rank == 1){
+        for (int i=0;i<i_max;i++){
+        printf("CURSADR: %f  r: %i  i: %i  \n", current_array[i], rank, i);
+    }
+        }
 
         // swap locally
         double *temp = old_array;
