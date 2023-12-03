752c6c6bef551bd0536efdf9a0de5243f6a06a84
DanieldenHartog <danielmdenhartog@gmail.com>
2023-12-03 18:21:15 +0100

    d

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -92,7 +92,7 @@ double *simulate(const int i_max, const int t_max, double *old_array,
     if (numprocs > 1) { // no comms necessary if only one process
         if(rank != 0) {
             // send current to master no need for non-blocking here
-            MPI_Send(current_array, i_max, MPI_DOUBLE, 0,  rank, MPI_COMM_WORLD);
+            MPI_Ssend(current_array, i_max, MPI_DOUBLE, 0,  rank, MPI_COMM_WORLD);
         }
         else {
             double buffer_array[i_max]; // buffer to store received array domains
