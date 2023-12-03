3d110b5a6c889d32987d9238213e4a8df82880f9
DanieldenHartog <danielmdenhartog@gmail.com>
2023-12-03 18:20:10 +0100

    dd

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -15,7 +15,7 @@
  * current_array: array of size i_max filled with data for t
  * next_array: array of size i_max. You should fill this with t+1
  */
-double *simulate1(const int i_max, const int t_max, double *old_array,
+double *simulate(const int i_max, const int t_max, double *old_array,
         double *current_array, double *next_array)
 {    
 
@@ -123,7 +123,7 @@ return current_array;
 // %%%%%%%%%%%%%%%%%%%%%%%%%%% Blocking receive %%%%%%%%%%%%%%%%%%%%%%%%%% //
 // %%%%%%%%%%%%%%%%%%%%%%%%%%% Blocking receive %%%%%%%%%%%%%%%%%%%%%%%%%% //
 
-double *simulate(const int i_max, const int t_max, double *old_array,
+double *simulate1(const int i_max, const int t_max, double *old_array,
         double *current_array, double *next_array)
 {    
 
