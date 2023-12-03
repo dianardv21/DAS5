b31dfa2bac4cfaf823727cb19b7301ec2a777937
DanieldenHartog <danielmdenhartog@gmail.com>
2023-12-03 14:36:02 +0100

    d

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -176,7 +176,7 @@ double *simulate(const int i_max, const int t_max, double *old_array,
                 }
                 
                 // copy relevant part of buffer to relevant part of current_array
-                memcpy(current_array + i, buffer_array + 3, 1*sizeof(double));
+                memcpy(&current_array + i, buffer_array + 3, 1*sizeof(double));
             }
         }
         
