4612ea07efa4e76f88323f20ebde1b0ba1bb7e97
DanieldenHartog <danielmdenhartog@gmail.com>
2023-12-03 18:00:06 +0100

    dd

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -4,6 +4,8 @@
 #include <mpi.h>
 #include <string.h>
 
+#include <windows.h> // for sleep()
+
 /* Add any global variables you may need. */
 
 /*
@@ -217,6 +219,7 @@ double *simulate(const int i_max, const int t_max, double *old_array,
                 // copy relevant part of buffer to relevant part of current_array
                 printf("s:%i, e:%i\n", start, end);
                 memcpy(current_array + start, buffer_array + start, (end-start+1)*sizeof(double));
+                sleep(1000);
             }
         }
     }
