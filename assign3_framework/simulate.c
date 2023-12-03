e3076f08b8648a884ef6788e13c6e177fee1d6e6
DanieldenHartog <danielmdenhartog@gmail.com>
2023-12-03 16:06:31 +0100

    d

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -167,7 +167,7 @@ double *simulate(const int i_max, const int t_max, double *old_array,
             double buffer_array[i_max]; // buffer to store received array domains
             for (int i = 1; i < numprocs; i++) {
                 // blocking receive data chunk, otherwise buffer_array gets overwritten
-                printf("\n%i\n\n", i);
+                printf("\n%i %i\n\n", i, numprocs);
                 MPI_Recv(&buffer_array, i_max, MPI_DOUBLE, i, i, MPI_COMM_WORLD, &stats[1]);
                 
                 // for each non-master process get domain and copy only its domain to current_array
