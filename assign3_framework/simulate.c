ee71c19d37198b0ea116e27e163b382963159c94
DanieldenHartog <73831941+DanieldenHartog@users.noreply.github.com>
2023-12-08 17:17:45 +0100

    Update simulate.c

--- assign3_framework/simulate.c
+++ assign3_framework/simulate.c
@@ -14,6 +14,7 @@ double *simulate(const int i_max, const int t_max, double *old_array,
                  double *current_array, double *next_array) {
         // alter commenting to choose implementation type
         // DEFAULT: fully blocking
+  
         return simulate_BLOCKING    (i_max, t_max, old_array,current_array,next_array);
         //return simulate_HALFBLOCKING(i_max, t_max, old_array,current_array,next_array);
         //return simulate_NONBLOCKING (i_max, t_max, old_array,current_array,next_array);
